eb4f4d8c1bd8ea694b3438014da683847dbf7cb3
Chi Chiu <ctsan005@ucr.edu>
2020-10-30 16:27:25 -0700

    test

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -3,7 +3,7 @@
 int get_block_size(){
     //return the block size you'd like to use 
     /*add your code here */
-    return 128;
+    return 129;
   
 }
 
@@ -29,59 +29,59 @@ int get_block_size(){
 int mydgetrf(double *A, int *ipiv, int n) 
 {
     /* add your code here */
-    int i, maxind;
-    double max;
-    for(i = 0; i < n; i++){
-        maxind = i;
-        max = fabs(A[i*n + i]);
-        int t;
-        for(t = i+1; t < n; t++){
-            if(fabs(A[t*n + i]) > max){
-                maxind = t;
-                max = fabs(A[t*n + i]);
-            }
-        }
+    // int i, maxind;
+    // double max;
+    // for(i = 0; i < n; i++){
+    //     maxind = i;
+    //     max = fabs(A[i*n + i]);
+    //     int t;
+    //     for(t = i+1; t < n; t++){
+    //         if(fabs(A[t*n + i]) > max){
+    //             maxind = t;
+    //             max = fabs(A[t*n + i]);
+    //         }
+    //     }
 
-        if(max == 0){
-            printf("LU factoration failed: coefficient matrix is singular");
-            return -1;
-        }
-        else{
-            if(maxind != i){
-                // save pivoting information
-                int temps= ipiv[i];
-                ipiv[i] = ipiv[maxind];
-                ipiv[maxind] = temps;
+    //     if(max == 0){
+    //         printf("LU factoration failed: coefficient matrix is singular");
+    //         return -1;
+    //     }
+    //     else{
+    //         if(maxind != i){
+    //             // save pivoting information
+    //             int temps= ipiv[i];
+    //             ipiv[i] = ipiv[maxind];
+    //             ipiv[maxind] = temps;
 
-                //swap row for matrix method 1
-                // int j;
-                // for(j = 0; j < n; j++){
-                //     double k;
-                //     k = A[i * n + j];
-                //     A[i * n + j] = A[maxind * n + j];
-                //     A[maxind * n + j] = k;
-                // }
+    //             //swap row for matrix method 1
+    //             // int j;
+    //             // for(j = 0; j < n; j++){
+    //             //     double k;
+    //             //     k = A[i * n + j];
+    //             //     A[i * n + j] = A[maxind * n + j];
+    //             //     A[maxind * n + j] = k;
+    //             // }
 
-                //swap row method 2 -- need to test which one is faster
-                double trow[n];
-                memcpy(trow, A + i * n, n*sizeof(double));
-                memcpy(A + i * n, A + maxind * n, n*sizeof(double));
-                memcpy(A + maxind * n, trow, n*sizeof(double));
-            }
+    //             //swap row method 2 -- need to test which one is faster
+    //             double trow[n];
+    //             memcpy(trow, A + i * n, n*sizeof(double));
+    //             memcpy(A + i * n, A + maxind * n, n*sizeof(double));
+    //             memcpy(A + maxind * n, trow, n*sizeof(double));
+    //         }
 
-        }
+    //     }
 
-        int j;
-        for(j = i + 1; j <n;j++){
-            A[j*n + i] = A[j*n + i] / A[i*n + i];
-            int k;
-            for(k =  i + 1; k < n; k++){
-                A[j*n + k] = A[j*n + k] - A[j*n + i] * A[i*n + k];
-            }
-        }
-    }
+    //     int j;
+    //     for(j = i + 1; j <n;j++){
+    //         A[j*n + i] = A[j*n + i] / A[i*n + i];
+    //         int k;
+    //         for(k =  i + 1; k < n; k++){
+    //             A[j*n + k] = A[j*n + k] - A[j*n + i] * A[i*n + k];
+    //         }
+    //     }
+    // }
 
-    return 0;
+    // return 0;
 }
 
 /**
@@ -275,83 +275,83 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
 {
 
     /* add your code here */
-    
-    // int i,j,k,ic,t, maxind;
-    // double max;
 
-    // for(ic = 0; ic <n;ic +=b){
-    //     for(i = ic; i < ic+b ; i++){
-    //         maxind = i;
-    //         max = fabs(A[i*n + i]);
-    //         for(t = i+1; t < n; t++){
-    //             if(fabs(A[t*n + i]) > max){
-    //                 maxind = t;
-    //                 max = fabs(A[t*n + i]);
-    //             }
-    //         }
+    int i,j,k,ic,t, maxind;
+    double max;
 
-    //         if(max == 0){
-    //             printf("LU factoration failed: coefficient matrix is singular");
-    //             return -1;
-    //         }
-    //         else{
-    //             if(maxind != i){
-    //                 // save pivoting information
-    //                 int temps= ipiv[i];
-    //                 ipiv[i] = ipiv[maxind];
-    //                 ipiv[maxind] = temps;
+    for(ic = 0; ic <n;ic +=b){
+        for(i = ic; i < ic+b ; i++){
+            maxind = i;
+            max = fabs(A[i*n + i]);
+            for(t = i+1; t < n; t++){
+                if(fabs(A[t*n + i]) > max){
+                    maxind = t;
+                    max = fabs(A[t*n + i]);
+                }
+            }
 
-    //                 //swap row for matrix method 1
-    //                 // int j;
-    //                 // for(j = 0; j < n; j++){
-    //                 //     double k;
-    //                 //     k = A[i * n + j];
-    //                 //     A[i * n + j] = A[maxind * n + j];
-    //                 //     A[maxind * n + j] = k;
-    //                 // }
+            if(max == 0){
+                printf("LU factoration failed: coefficient matrix is singular");
+                return -1;
+            }
+            else{
+                if(maxind != i){
+                    // save pivoting information
+                    int temps= ipiv[i];
+                    ipiv[i] = ipiv[maxind];
+                    ipiv[maxind] = temps;
 
-    //                 //swap row method 2 -- need to test which one is faster
-    //                 double trow[n];
-    //                 memcpy(trow, A + i * n, n*sizeof(double));
-    //                 memcpy(A + i * n, A + maxind * n, n*sizeof(double));
-    //                 memcpy(A + maxind * n, trow, n*sizeof(double));
-    //             }
+                    //swap row for matrix method 1
+                    // int j;
+                    // for(j = 0; j < n; j++){
+                    //     double k;
+                    //     k = A[i * n + j];
+                    //     A[i * n + j] = A[maxind * n + j];
+                    //     A[maxind * n + j] = k;
+                    // }
 
-    //         }
+                    //swap row method 2 -- need to test which one is faster
+                    double trow[n];
+                    memcpy(trow, A + i * n, n*sizeof(double));
+                    memcpy(A + i * n, A + maxind * n, n*sizeof(double));
+                    memcpy(A + maxind * n, trow, n*sizeof(double));
+                }
 
-    //         for(j = i + 1; j <n;j++){
-    //             A[j*n + i] = A[j*n + i] / A[i*n + i];
+            }
 
-    //             for(k = i + 1; k < i + b; k++){
-    //                 A[j*n + k] = A[j*n + k] - A[j*n + i] * A[i*n + k];
-    //             }
-    //         }
-    //     }
+            for(j = i + 1; j <n;j++){
+                A[j*n + i] = A[j*n + i] / A[i*n + i];
 
-    //     //update A(ib:end, end+1:n), basically same method as before, use the value store in A(ib:n, ib:end)
-    //     register double total;
-    //     //end = ic + b
-    //     for(i = ic; i < ic + b; i++){
-    //         for(j= ic;j < n;j++){
-    //             total = 0;
-    //             for(k = ic; k < i; k++){
-    //                 // A[i*n - j] -= A[i*n + k] * A[k*n + j];
-    //                 total += A[i*n + k] * A[k*n + j];
-    //             }
-    //             A[i*n + j] -= total;
-    //         }
-    //     }
+                for(k = i + 1; k < i + b; k++){
+                    A[j*n + k] = A[j*n + k] - A[j*n + i] * A[i*n + k];
+                }
+            }
+        }
 
-    //     for(i = ic; i < n;i++){
-    //         for(j = ic; j < n; j++){
+        //update A(ib:end, end+1:n), basically same method as before, use the value store in A(ib:n, ib:end)
+        register double total;
+        //end = ic + b
+        for(i = ic; i < ic + b; i++){
+            for(j= ic;j < n;j++){
+                total = 0;
+                for(k = ic; k < i; k++){
+                    // A[i*n - j] -= A[i*n + k] * A[k*n + j];
+                    total += A[i*n + k] * A[k*n + j];
+                }
+                A[i*n + j] -= total;
+            }
+        }
 
-    //         }
-    //     }
+        for(i = ic; i < n;i++){
+            for(j = ic; j < n; j++){
 
-    //     // update A(end + 1: n , end + 1 : n)
-    //     // end = ic + b
-    //     mydgemm(A, A, A,n, ic + b, ic + b, ic, b);
-    // }
+            }
+        }
+
+        // update A(end + 1: n , end + 1 : n)
+        // end = ic + b
+        mydgemm(A, A, A,n, ic + b, ic + b, ic, b);
+    }
 
     
 
