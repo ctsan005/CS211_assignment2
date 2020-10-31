e903de7342165c1cf645df3347bb953da892e973
Chi Chiu <ctsan005@ucr.edu>
2020-10-30 18:50:24 -0700

    test

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -166,9 +166,9 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
     int block_size = 3;
 
     for(i1 = i; i1 < n; i1 += b){
-        printf("i = %i", i1);
+        // printf("i = %i", i1);
         for(j1 = j;j1 < n; j1 += b){
-            printf("j = %i", j1);
+            // printf("j = %i", j1);
             for(k1 = k; k1 < k1 + b; k1 += b){
 
                 for (ic=i1; ic<(ic + b); ic+=block_size){
