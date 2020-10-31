c44337667f87d5e707a6fcb850feee17808b616b
Chi Chiu <ctsan005@ucr.edu>
2020-10-30 20:20:58 -0700

    test

--- test/verification.c
+++ test/verification.c
@@ -23,8 +23,8 @@
 int test_all_lu_functions()
 {
     int block_size=get_block_size();
-    int ni, nList[] = {103, 142, 3000, 4000, 5000};
-    for (ni = 0; ni < 2; ni++) {
+    int ni, nList[] = {1000, 142, 3000, 4000, 5000};
+    for (ni = 0; ni < 1; ni++) {
         int n = nList[ni], i, j;
 
         // feel free to modify the block size by yourself.
