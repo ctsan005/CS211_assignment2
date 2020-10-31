40bb83a01c348d55f435795d3e1119a43c4382e5
Chi Chiu <ctsan005@ucr.edu>
2020-10-30 19:11:06 -0700

    test

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -227,6 +227,7 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
                         for (kc=k1; kc<(kc + b); kc+=block_size){
                             if(kc >= n){
                                 printf("kc error\n");
+                                printf(kc); 
                                 return;
                             }
                             for(m = 0; m < block_size; m++){
